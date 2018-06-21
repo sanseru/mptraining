5a906186eed1521463b618d996da2dfababdd551
sanseru <sanseru1@yahoo.com>
2018-06-21 10:46:19 +0700

    2nd Commit

--- koneksi.php
+++ koneksi.php
@@ -1,11 +1,11 @@
 <?php
-//$host="localhost";
-//$user="root";
-//$pass="";
+$host="localhost";
+$user="root";
+$pass="";
 //$db="dbujian";
-$host ="localhost";
-$user="ciremac_email";
-$pass="J0h4rB4ru";
+// $host ="localhost";
+// $user="ciremac_email";
+// $pass="J0h4rB4ru";
 $db="dbujian";
 
 $koneksi=mysqli_connect($host,$user,$pass,$db);
